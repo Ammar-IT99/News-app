bc5c03c736e05d968187e9d42ba6c2111dee6174
Ammar-IT99 <elkingamar44@gmail.com>
2024-06-22 19:08:14 +0300

    edit category_dm.dart.

--- lib/model/category_dm.dart
+++ lib/model/category_dm.dart
@@ -12,10 +12,9 @@ static List<CategoryDM>getCategories() {
    return [
      CategoryDM(id: 'Sports', title: 'Sports', imagePath: 'assets/images/ball.png', color: MyTheme.redColor),
      CategoryDM(id: 'business', title: 'business', imagePath: 'assets/images/bussines.png', color: MyTheme.brownColor),
-     CategoryDM(id: 'environment', title: 'environment', imagePath: 'assets/images/environment.png', color: MyTheme.blueColor),
      CategoryDM(id: 'health', title: 'health', imagePath: 'assets/images/health.png', color: MyTheme.pinkColor),
-     CategoryDM(id: 'Politics', title: 'Politics', imagePath: 'assets/images/Politics.png', color: MyTheme.darkBlueColor),
      CategoryDM(id: 'science', title: 'science', imagePath: 'assets/images/science.png', color: MyTheme.yellowColor),
+
 
    ];
 }
